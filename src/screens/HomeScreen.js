552724ac020b23d0dda9bc9e1c503900dbf0cc61
joshuatsai <joshuatsai1213@gmail.com>
2024-04-11 18:51:51 +0800

    use theme

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Box } from "@gluestack-ui/themed"
 import BookList from "../components/BookList";
 import sections from "../json/JournalContent.json";
+import { useTheme } from '@react-navigation/native';
 
 const HomeScreen = ({ navigation }) => {
+    const { colors } = useTheme();
+
     return (
         <>
             <Box bgColor="white" height="100%">
-                <Box bgColor="black" height={200}>
+                <Box bgColor={colors.purple} height={200}>
                 </Box>
                 <Box bgColor="white" height="100%">
                     <BookList
